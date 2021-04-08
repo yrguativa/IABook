f340426feb12a94e195c91f2b07007df0d2690dc
Yilmer Guativa <yilmer.1994.03@hotmail.com>
2021-04-08 16:05:07 -0500

    fix => Algoritmo búsqueda informada - backtracking

--- BusquedaInformada/ple_backtracking.py
+++ BusquedaInformada/ple_backtracking.py
@@ -4,23 +4,26 @@ def backtracking(variables, rango_variables, optimo, profundidad):
     max = rango_variables[profundidad][1]
     for v in range(min, max):
         variables[profundidad] = v
-        if profundidad < len(variables) - 1:
-            # Es completable si no incumple ninguna restricción
+        #estamos en el bloque de código para evaluar las ramas
+        if profundidad < len(variables) -1:
+            #Es completable si no incumple ninguna restricción
             if es_completable(variables):
-                optimo = backtracking(
-                    variables[:], rango_variables, optimo, profundidad + 1)
-            else:
-                # Estamos en una hoja. Comprobamos solución
-                sol = evalua_solucion(variables)
-                if sol > evalua_solucion(optimo) and es_completable(variables):
-                    optimo = (variables[0], variables[1])
+                optimo = backtracking(variables[:], rango_variables, optimo, profundidad +1)
+        #este bloque de código es para las hojas
+        else:
+            #comprobamos que la solucion sea optima para la hoja
+            sol = evalua_solucion(variables)
+            #si la solucion es mayor que optimo en su estado inicial y cumple que se puede completar con las restricciones dadas
+            # entonces se asigna a optimo los valores actuales de la solucion
+            if sol > evalua_solucion(optimo) and es_completable(variables):
+                optimo = (variables[0], variables[1])
+                
     return optimo
-
 
 def evalua_solucion(variables):
     x1 = variables[0]
     x2 = variables[1]
-    val = (12-6) * x1 + (8-4) * x2
+    val = (12 - 6) * x1 + (8 - 4) * x2
     return val
 
 
@@ -39,11 +42,11 @@ if __name__ == "__main__":
     # valores de las variables x1 y x2
     variables = [0, 0]
     # rangos de las variables x1 y x2
-    rango_variables = [(0 , 51) , (0 , 76)]
-    # mejor solución encontrada
+    rango_variables = [(0, 51), (0, 76)]
+    # mejor solucion encontrada
     optimo = (0, 0)
     sol = backtracking(variables[:], rango_variables, optimo, 0)
-    print("Mejor solución:")
-    print(str(sol[0]) + " Pantalones.")
-    print(str(sol[1]) + " Camisetas.")
-    print("Beneficio: " + str(evalua_solucion(sol)))
+    print("Mejor solucion: ")
+    print(str(sol[0]) + " Pantalones")
+    print(str(sol[1]) + " Camisetas")
+    print("Beneficio " + str(evalua_solucion(sol)))
